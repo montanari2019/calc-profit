696842545b1865baf6a72e3975c6bca286a31219
montanari2019 <ikaro.montanari31@gmail.com>
2022-08-22 22:54:12 -0400

    header finalizado e resposivo

--- src/@router.tsx
+++ src/@router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./scenes/home/Home";
+import { Profit } from "./scenes/profit/Profit";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home/>} />
+        <Route path="/profit" element={<Profit/>}/>
       </Routes>
     </BrowserRouter>
   )
--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -6,18 +6,18 @@ import styleNavBar from "./NavBar.module.css";
 const linksNavigation = [
   {
     nameLink: "Home",
-    url: "#",
+    url: "/",
   },
   {
     nameLink: "Calcular Sobras",
-    url: "#",
+    url: "/profit",
   },
   {
     nameLink: "Vizualiar Sobras",
     url: "#",
   },
   {
-    nameLink: "Sobra",
+    nameLink: "Sobre",
     url: "#",
   },
 ];
@@ -26,39 +26,56 @@ export function NavBar() {
   const [menuCheck, setMenuCheck] = useState<Boolean>(false);
 
   function handleMenuClick() {
-    if(menuCheck === true){
-        setMenuCheck(false);
-        console.log("Menu fechado")
-    }else if(menuCheck === false){
-        setMenuCheck(true);
-        console.log("Menu aberto")
+    if (menuCheck === true) {
+      setMenuCheck(false);
+      console.log("Menu fechado");
+    } else if (menuCheck === false) {
+      setMenuCheck(true);
+      console.log("Menu aberto");
     }
     // console.log("clickando no menu");
   }
 
   return (
     <div>
-      <List
-        className={styleNavBar.icon}
-        onClick={handleMenuClick}
-        color="#323238"
-        size={36}
-      />
 
-      <section className={ menuCheck === true ? styleNavBar.menuOpen : styleNavBar.menuClose}>
-        <X className={styleNavBar.iconClose} onClick={handleMenuClick} color="#E1E1E6" size={36} />
+        <section className={styleNavBar.navDesktop}>
+            {linksNavigation.map((linksNav) =>{
+                return(
+                    <Link className={styleNavBar.linkNavigation} key={linksNav.nameLink} to={linksNav.url}>{linksNav.nameLink}</Link>
+                )
+            })}
+        </section>
 
-        <div className={styleNavBar.menuItem}>
-          {linksNavigation.map((links) => (
-            <Link
-              key={links.nameLink}
-              className={styleNavBar.linkNavigation}
-              to={links.url}
-            >
-              {links.nameLink}
-            </Link>
-          ))}
-        </div>
+        {/* Menu nav bar */}
+      <section className={styleNavBar.navBar}>
+        <List
+          className={styleNavBar.icon}
+          onClick={handleMenuClick}
+          color="#323238"
+          size={36}
+        />
+
+        <section className={menuCheck === true ? styleNavBar.menuOpen : styleNavBar.menuClose}>
+          <X
+            className={styleNavBar.iconClose}
+            onClick={handleMenuClick}
+            color="#E1E1E6"
+            size={36}
+          />
+
+          <div className={styleNavBar.menuItem}>
+            {linksNavigation.map((links) => (
+              <Link
+                key={links.nameLink}
+                className={styleNavBar.linkNavigation}
+                to={links.url}
+              >
+                {links.nameLink}
+              </Link>
+            ))}
+          </div>
+        </section>
       </section>
     </div>
   );
