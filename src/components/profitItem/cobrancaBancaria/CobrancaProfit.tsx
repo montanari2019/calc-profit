863b33a1c2dd3162f68120cd144d5db9681293bf
montanari2019 <ikaro.montanari31@gmail.com>
2022-08-30 13:40:35 -0400

    style profit calc open ok

--- src/components/profitItem/cobrancaBancaria/CobrancaProfit.tsx
+++ src/components/profitItem/cobrancaBancaria/CobrancaProfit.tsx
@@ -1,19 +1,23 @@
 import { Plus } from "phosphor-react";
 import { useState } from "react";
 import { InputProfitItem } from "../inputProfitItem/InputProfitItem";
+import { ResultButtonItem } from "../resultButtonItem/ResultButtonItem";
 import styleCobrancaProfit from "./CobrancaProfit.module.css";
 
 export function CobrancaProfit() {
   const [cardOpen, setCardOpen] = useState<Boolean>(false);
 
   return (
-   
     <div>
       <div className={styleCobrancaProfit.cardBody}>
         <section className={styleCobrancaProfit.cardOpen}>
           <section className={styleCobrancaProfit.ajustButtonCard}>
             <div className={styleCobrancaProfit.iconOpen}>
-              <Plus color="#E1E1E6" size={27}  className={styleCobrancaProfit.iconPlusOpen}/>
+              <Plus
+                color="#E1E1E6"
+                size={27}
+                className={styleCobrancaProfit.iconPlusOpen}
+              />
             </div>
 
             <strong className={styleCobrancaProfit.strong}>
@@ -22,15 +26,30 @@ export function CobrancaProfit() {
           </section>
         </section>
 
-        <InputProfitItem labelName={"Ikaro"} inputType={"text"} placeholderName={"teste"}/>
-        <InputProfitItem labelName={"Kaiury"} inputType={"password"} placeholderName={"teste"}/>
-        <InputProfitItem labelName={"Adam"} inputType={"email"} placeholderName={"teste"}/>
-        
+        <section className={styleCobrancaProfit.sectionDisplay}>
+          <div>
+            <InputProfitItem
+              labelName={"Ikaro"}
+              inputType={"text"}
+              placeholderName={"teste"}
+            />
+            <InputProfitItem
+              labelName={"Kaiury"}
+              inputType={"password"}
+              placeholderName={"teste"}
+            />
+            <InputProfitItem
+              labelName={"Adam"}
+              inputType={"email"}
+              placeholderName={"teste"}
+            />
+          </div>
 
-        
+          <div>
+            <ResultButtonItem/>
+          </div>
+        </section>
       </div>
-
-    
     </div>
   );
 }
---
+++ src/components/profitItem/resultButtonItem/ResultButtonItem.tsx
@@ -0,0 +1,17 @@
+import styleResultButtonItem from "./ResultButtonItem.module.css"
+
+export function ResultButtonItem(){
+    return(
+        <div className={styleResultButtonItem.container}>
+            <strong>Valor projetado a ser devolvido</strong>
+            <div className={styleResultButtonItem.result}>
+                <strong>182.658,30</strong>
+            </div>
+
+            <div className={styleResultButtonItem.buttonDisplay}>
+                <button className={styleResultButtonItem.buttonAddResult} type="button">Adicionar Resultado</button>
+                <button className={styleResultButtonItem.buttonDeleteResult} type="button">Excluir simulação</button>
+            </div>
+        </div>
+    )
+}
